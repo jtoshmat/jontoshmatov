42d7714e8b280f1029d5544679bd76ec88932fc1
Jon Toshmatov <jontoshmatov@yahoo.com>
2015-11-06 19:21:49 -0500

    welcome view

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace app\Http\Controllers\Auth;
 
-use App\User;
+use app\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use app\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
@@ -41,9 +41,13 @@ class AuthController extends Controller
      */
     protected function validator(array $data)
     {
+	    $data['slug'] = str_slug($data['first_name'].' '.$data['middle_name'].' '.$data['last_name'].' '.str_random(10), "-");
         return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
+            'first_name' => 'required|max:255',
+            'middle_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'email' => 'email|max:255',
+	        'slug'=>'required|string|unique:users|min:2',
             'password' => 'required|confirmed|min:6',
         ]);
     }
@@ -56,9 +60,13 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+	    $data['slug'] = str_slug($data['first_name'].' '.$data['middle_name'].' '.$data['last_name'].' '.str_random(10), "-");
+	    return User::create([
+            'first_name' => $data['first_name'],
+            'middle_name' => $data['middle_name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
+            'slug' => $data['slug'],
             'password' => bcrypt($data['password']),
         ]);
     }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,67 +25,6 @@
         return view('welcome');
     });
 
-    Route::get('/home', function () {
-        return view('home');
-    });
 
-    Route::get('/aboutme', 'PublicController@aboutme');
-
-
-    // Authentication routes...
-    Route::get('auth/login', 'Auth\AuthController@getLogin');
-    Route::post('auth/login', 'Auth\AuthController@postLogin');
-    Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-    // Registration routes...
-    Route::get('auth/register', 'Auth\AuthController@getRegister');
-    Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-    $router->get("/awesome/sauce", ['middleware' => 'role:student,admin'], function () {
-        echo('Awesome Sauce');
-    });
-
-    Route::group(['middleware' => 'auth'], function($router) {
-        /*
-         * All authenticated routes are registered here inside of auth
-         */
-
-        Route::any('users/members', 'UsersController@members');
-        Route::any('users/roles', 'UsersController@roles');
-
-        Route::group(['middleware' => 'role:'.Config::get('myroutes.routes.role')], function ($router) {
-
-            Route::get("users/member/{id}/{action}", [
-                "as"   => "users/member",
-                "uses" => "UsersController@member",
-            ],function($id, $action){
-                return $id;
-            })->where('id', '[0-9]+')->where('action','view');
-        });
-
-
-        //All Admin Tools here in this group
-        Route::group(['middleware' => 'role:admin'], function ($router) {
-            Route::any('admin/uploadcsv', 'AdminToolsController@uploadcsv');
-            Route::any('admin/playground', 'AdminTestController@uploadImage');
-        });
-
-        Route::any('users/member/{id}/update', 'UsersController@memberUpdate')->where('id', '[0-9]+');
-        Route::any('users/member/{id}/delete', 'UsersController@memberDelete')->where('id', '[0-9]+');
-        Route::any('user/{id}/view', 'UsersController@user')->where('id', '[0-9]+');
-        Route::any('districts', 'DistrictsController@index');
-        Route::any('district/{id}/view', 'DistrictsController@district')->where('id', '[0-9]+');
-        Route::any('organizations', 'OrganizationsController@index')->where('id', '[0-9]+');
-        Route::any('organization/{id}/view', 'OrganizationsController@organization')->where('id', '[0-9]+');
-        Route::any('groups', 'GroupsController@index');
-        Route::any('group/{id}/view', 'GroupsController@group')->where('id', '[0-9]+');
-        Route::any('guardians', 'UsersController@guardian');
-
-        //Composer for sidebars and user specific contents
-        View::composer('partials.sidebar','app\cmwn\Users\UserSpecificRepository');
-
-
-
-    });
 
 
